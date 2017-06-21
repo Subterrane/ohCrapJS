46940b839a8d73cf39643e6c7ce5b1c4295b5c9d
Andrew Hughes <andrew.hughes2610@gmail.com>
2017-06-21 14:04:22 +0100

    updated to add new functions based on serverity

--- ohCrap.js
+++ ohCrap.js
@@ -5,3 +5,19 @@ function ohCrap(string){
         console.error("[Error]" + err);
     }
 }
+
+function ohDear(string){
+    try{
+        console.warn(string);
+    }catch(err){
+        console.warn("[Warn]" + err);
+    }
+}
+
+function meh(string){
+    try{
+        console.log(string);
+    }catch(err){
+        console.log("[Log]" + err);
+    }
+}
